de80f83bd2af700b2b4073e6fa8433a1c5c67d2c
Manish sahu <sanjusahu7326@gmail.com>
2024-07-25 09:16:14 +0530

    module

--- index.js
+++ index.js
@@ -1,1 +1,6 @@
+const math = require("./math");
+
 console.log("Hwllo word");
+// console.log(math(5,5));
+console.log(math.aadd(4,8));
+console.log(math.ssub(8,4));
---
+++ math.js
@@ -0,0 +1,18 @@
+function add(a,b){
+    return a+b;
+}
+
+function sub(a,b){
+
+    return a-b;
+}
+
+// module.exports =add;
+
+// if  i am call two function 
+
+
+module.exports ={
+    aadd:add,
+    ssub:sub
+}
